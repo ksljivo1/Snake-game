12f6577bdeb7cfcc6af64da8cf4d1195fbc64ece
ksljivo1 <ksljivo1@etf.unsa.ba>
2024-05-08 00:45:51 +0200

    implemented growing logic

--- food.js
+++ food.js
@@ -3,7 +3,6 @@ class Food {
         this.position = {x: Math.floor(Math.random() * 580), y: Math.floor(Math.random() * 280)};
         this.position.x = Math.ceil(this.position.x  / 10) * 10;
         this.position.y = Math.ceil(this.position.y  / 10) * 10;
-        this.alive = true;
         this.canvas = document.getElementById("canvas");
     }
 
@@ -12,14 +11,9 @@ class Food {
     }
 
     show() {
-        if(!this.alive) return;
         var ctx = this.canvas.getContext("2d");
         ctx.fillStyle = "white";
         ctx.fillRect(this.position.x, this.position.y, 10, 10);
         ctx.stroke();
     }
-
-    /*dead() {
-        this.alive = false;
-    }*/
 }
--- snake.js
+++ snake.js
@@ -3,22 +3,29 @@ class Snake {
         this.position = {x: Math.floor(Math.random() * 580) , y: Math.floor(Math.random() * 280)};
         this.position.x = Math.ceil(this.position.x  / 10) * 10;
         this.position.y = Math.ceil(this.position.y  / 10) * 10;
-        this.lives = 5;
-        this.length = [this.position]
+        this.length = 1;
+        this.size = [];
         this.canvas = document.getElementById("canvas");
-        this.direction = {right: 1, left: 0, up: 0, down: 0}
+        this.direction = {right: 1, left: 0, up: 0, down: 0};
     }
 
     show() {
         var ctx = this.canvas.getContext("2d");
         ctx.fillStyle = "white";
-        ctx.fillRect(this.position.x, this.position.y, 10, 10);
-        ctx.stroke();
-        console.log(this.position);
+        for(var i = 0; i < this.length; i++) {
+            console.log("now;")
+            ctx.fillRect(this.size[i].x, this.size[i].y, 10, 10);
+            ctx.stroke();
+            console.log(i + "jebem te " + this.size[i].x  + " " + this.size[i].y + "\n");
+        }
     }
 
     eats(food) {
         return this.position.x === food.getPosition().x && this.position.y === food.getPosition().y;
+    }
+
+    grow() {
+        this.length++;
     }
 
     async update() {
@@ -49,7 +56,8 @@ class Snake {
                 this.direction.right = 0;
                 this.direction.left = 1;
             }
-            });
+        });
+        this.size.unshift({x: this.position.x, y: this.position.y});
         if(this.direction.right === 1) {
             this.buttonRightPressed();
         }
@@ -62,6 +70,7 @@ class Snake {
         else  {
             this.buttonDownPressed();
         }
+        //console.log(this.position);
         this.show();
     }
 
@@ -82,3 +91,19 @@ class Snake {
     }
 
 }
+
+Array.prototype.insertAtIndex = function(index, element) {
+    // Check if the index is valid
+    if (index < 0 || index > this.length) {
+        console.error("Index out of bounds");
+        return;
+    }
+    
+    // Shift elements to the right to make space for the new element
+    for (let i = this.length; i > index; i--) {
+        this[i] = this[i - 1];
+    }
+    
+    // Insert the element at the specified index
+    this[index] = element;
+};
