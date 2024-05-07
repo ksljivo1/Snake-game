95a3e985b74a9cf087e6fd7d5d389135ce5f8390
ksljivo1 <ksljivo1@etf.unsa.ba>
2024-05-07 02:20:02 +0200

    basic game logic implemented

---
+++ food.js
@@ -0,0 +1,25 @@
+class Food {
+    constructor() {
+        this.position = {x: Math.floor(Math.random() * 580), y: Math.floor(Math.random() * 280)};
+        this.position.x = Math.ceil(this.position.x  / 10) * 10;
+        this.position.y = Math.ceil(this.position.y  / 10) * 10;
+        this.alive = true;
+        this.canvas = document.getElementById("canvas");
+    }
+
+    getPosition() {
+        return this.position;
+    }
+
+    show() {
+        if(!this.alive) return;
+        var ctx = this.canvas.getContext("2d");
+        ctx.fillStyle = "white";
+        ctx.fillRect(this.position.x, this.position.y, 10, 10);
+        ctx.stroke();
+    }
+
+    dead() {
+        this.alive = false;
+    }
+}
--- snake.js
+++ snake.js
@@ -1,8 +1,10 @@
 class Snake {
     constructor() {
-        this.position = {x: Math.floor(Math.random() * 580), y: Math.floor(Math.random() * 280)};
+        this.position = {x: Math.floor(Math.random() * 580) , y: Math.floor(Math.random() * 280)};
+        this.position.x = Math.ceil(this.position.x  / 10) * 10;
+        this.position.y = Math.ceil(this.position.y  / 10) * 10;
         this.lives = 5;
-        this.length = 1;
+        this.length = [this.position]
         this.canvas = document.getElementById("canvas");
         this.direction = {right: 1, left: 0, up: 0, down: 0}
     }
@@ -12,73 +14,71 @@ class Snake {
         ctx.fillStyle = "white";
         ctx.fillRect(this.position.x, this.position.y, 10, 10);
         ctx.stroke();
+        console.log(this.position);
+    }
+
+    eats(food) {
+        return this.position.x === food.getPosition().x && this.position.y === food.getPosition().y;
     }
 
     async update() {
         var ctx = this.canvas.getContext("2d");
-        while(true) {
-            ctx.clearRect(0, 0, 600, 300);
-            document.addEventListener('keyup', (event) => {
-                // Check if the key released is the "Up" arrow key
-                if (event.key === 'ArrowUp') {
-                    this.direction.up = 1;
-                    this.direction.down = 0;
-                    this.direction.right = 0;
-                    this.direction.left = 0;
-                }
-                else if (event.key === 'ArrowDown') {
-                    this.direction.up = 0;
-                    this.direction.down = 1;
-                    this.direction.right = 0;
-                    this.direction.left = 0;
-                }
-                else if (event.key === 'ArrowRight') {
-                    this.direction.up = 0;
-                    this.direction.down = 0;
-                    this.direction.right = 1;
-                    this.direction.left = 0;
-                }
-                else {
-                    this.direction.up = 0;
-                    this.direction.down = 0;
-                    this.direction.right = 0;
-                    this.direction.left = 1;
-                }
-              });
-            if(this.direction.right === 1) {
-                this.buttonRightPressed();
+        ctx.clearRect(0, 0, 600, 300);
+        document.addEventListener('keyup', (event) => {
+            if (event.key === 'ArrowUp') {
+                this.direction.up = 1;
+                this.direction.down = 0;
+                this.direction.right = 0;
+                this.direction.left = 0;
             }
-            else if(this.direction.left === 1) {
-                this.buttonLeftPressed();
+            else if (event.key === 'ArrowDown') {
+                this.direction.up = 0;
+                this.direction.down = 1;
+                this.direction.right = 0;
+                this.direction.left = 0;
             }
-            else if(this.direction.up === 1) {
-                this.buttonUpPressed();
+            else if (event.key === 'ArrowRight') {
+                this.direction.up = 0;
+                this.direction.down = 0;
+                this.direction.right = 1;
+                this.direction.left = 0;
             }
-            else  {
-                this.buttonDownPressed();
+            else {
+                this.direction.up = 0;
+                this.direction.down = 0;
+                this.direction.right = 0;
+                this.direction.left = 1;
             }
-            this.show();
-            await sleep(150);
+            });
+        if(this.direction.right === 1) {
+            this.buttonRightPressed();
         }
+        else if(this.direction.left === 1) {
+            this.buttonLeftPressed();
+        }
+        else if(this.direction.up === 1) {
+            this.buttonUpPressed();
+        }
+        else  {
+            this.buttonDownPressed();
+        }
+        this.show();
     }
 
     buttonUpPressed() {
-        this.position.y -= 7;
+        this.position.y -= 10;
     }
 
     buttonDownPressed() {
-        this.position.y += 7;
+        this.position.y += 10;
     }
 
     buttonRightPressed() {
-        this.position.x += 7;
+        this.position.x += 10;
     }
 
     buttonLeftPressed() {
-        this.position.x -= 7;
+        this.position.x -= 10;
     }
 
-}
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
 }
